2fd2c4de7a857a1606b1fc7d48521c26273e9457
sipiteno <sales@sipiteno.com>
2026-07-19 20:13:44 +0300

    security: add COOP, COEP, Trusted Types CSP to signals pseo-site

--- pseo-site/next.config.ts
+++ pseo-site/next.config.ts
@@ -233,7 +233,17 @@ const nextConfig: NextConfig = {
               "font-src 'self'",
               "connect-src 'self' https://eu.i.posthog.com https://eu.posthog.com https://eu-assets.i.posthog.com https://api.resend.com https://scripts.refgrowcdn.com",
               "frame-ancestors 'none'",
+              "upgrade-insecure-requests",
+              "require-trusted-types-for 'script'",
             ].join("; "),
+          },
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin",
+          },
+          {
+            key: "Cross-Origin-Embedder-Policy",
+            value: "credentialless",
           },
         ],
       },
